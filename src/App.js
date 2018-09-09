6e224a2760066e4f38de031767e7c8bea9e56183
Dina <dcaraballo26@gmail.com>
2018-09-09 10:30:45 -0600

    Rename files and component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import data from './mockData';
-import CurrWeather from './CurrWeather';
+import CurrentWeather from './CurrentWeather';
 import SevenHour from './SevenHour';
 import TenDay from './TenDay';
 
@@ -27,7 +27,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to Weathrly</h1>
         </header>
-        <CurrWeather data={this.state.data} />
+        <CurrentWeather data={this.state.data} />
         <SevenHour data={this.state.data} />
         <TenDay data={this.state.data} />
       </div>
