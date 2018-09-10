73bc05e4732b903b55c65bd93243d75247d50981
Cole Hart <cole@colehart.com>
2018-09-10 14:38:09 -0600

    Refactor WeatherCard render function to reflect refactor

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -18,12 +18,15 @@ export default class WeatherCard extends Component {
   }
 
   render() {
+    const imgPath = `./assets/images/${this.state.weatherIcon}.png`
+
+    console.log(imgPath)
     if (this.state.hour) {
       console.log(this.state)
       return (
         <section className="WeatherCard">
           <h2>{this.state.hour}</h2>
-          <img href="{this.state.weatherIcon}.svg" alt="weather icon"/>
+          <img src={imgPath} alt="weather icon"/>
           <h2>{this.state.projectedHrTempF}&deg; F / {this.state.projectedHrTempC}&deg; C</h2>
         </section>
       )
@@ -31,11 +34,9 @@ export default class WeatherCard extends Component {
       return (
         <section className="WeatherCard">
           <h2>{this.state.weekday}</h2>
-          <img href="{this.state.weatherIcon}.svg" alt="weather icon"/>
-          <h1><span className="weatherHiLo">High:</span> {this.state.projectedHigh}</h1>
-          <h2>{this.state.weeknight}</h2>
-          <img href="{this.state.weatherIcon}.svg" alt="weather icon"/>
-          <h1><span className="weatherHiLo">Low:</span> {this.state.projectedLow}</h1>
+          <img src={imgPath} alt="weather icon"/>
+          <h3><span className="weatherHiLo">High:</span> {this.state.highTempF}&deg; F / {this.state.highTempC}&deg; C</h3>
+          <h3><span className="weatherHiLo">Low:</span> {this.state.lowTempF}&deg; F / {this.state.lowTempC}&deg; C</h3>
         </section>
       )
     }
