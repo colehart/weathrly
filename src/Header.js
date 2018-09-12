0b062200a4f927d8b393a385ea3ad16febb4d562
Cole Hart <cole@colehart.com>
2018-09-12 17:32:52 -0600

    Change Header message

--- src/Header.js
+++ src/Header.js
@@ -8,8 +8,8 @@ export default function Header(props) {
 
   return (
     <header className="app-header" aria-label="Weathrly application header">
-      <h1 className="app-title" aria-label="Application title">Welcome back to Weathrly!</h1>
-      <p className="app-intro" aria-label="App description">Please enter your city and state or five-digit zip code to get started.</p>
+      <h1 className="app-title" aria-label="Application title">Weathrly</h1>
+      <p className="app-intro" aria-label="App description">Please enter a new city and state or five-digit zip code to change your location.</p>
       <section className="header-form">
         <Search location={ location } addLocation={props.addLocation} />
       </section>
